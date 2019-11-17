0da8bbc710cf1937a94a533b8442b586358a63f0
Maika Kailani <queenmab051@gmail.com>
2019-11-17 21:28:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.collect do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms (room)
+  find_rooms = []
+  room.each_with_index do |name, number|
+    find_rooms << "Hello, #{name}! You have been assigned to room #{number+1}!"
+  end
+  find_rooms
+end
