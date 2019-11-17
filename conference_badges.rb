df104605735081aa039ef93538d739161a444f30
Maika Kailani <queenmab051@gmail.com>
2019-11-17 21:29:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 def assign_rooms(room)
   find_rooms = []
   room.each_with_index do |name, number|
-    find_rooms << "Hello, #{name}! You have been assigned to room #{number+1}!"
+    find_rooms << "Hello, #{name}! You'll be assigned to room #{number+1}!"
   end
   find_rooms
 end
