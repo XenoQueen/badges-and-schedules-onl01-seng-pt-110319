7d1a9b25375137796c33c0fd41a5f02fb0e41347
Maika Kailani <queenmab051@gmail.com>
2019-11-17 21:28:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,11 @@ def badge_maker(name)
     "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  names.collect do |name|
-    badge_maker(name)
-  end
-end
+#def batch_badge_creator(names)
+ # names.collect do |name|
+  #  badge_maker(name)
+  #end
+#end
 
 def assign_rooms(room)
   find_rooms = []
